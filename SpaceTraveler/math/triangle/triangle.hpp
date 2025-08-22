da41d5cc293db837140a51a3248498fa23120c88
MaksymVarivodin <maksym.varivodin@gmail.com>
2025-08-23 02:03:10 +0300

    Add BackgroundThread class and triangle data structure.

---
+++ SpaceTraveler/math/triangle/triangle.hpp
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include <cstdint>
+#include <memory>
+#include "../point/point.hpp"
+
+namespace SpaceEngine
+{
+    enum vertex: uint8_t
+    {
+        A,
+        B,
+        C
+    };
+
+    enum rgba: uint8_t
+    {
+        r,
+        g,
+        b,
+        a
+    };
+
+    using triangle_c = point<uint8_t>;
+
+    template <class T>
+    class triangle
+    {
+        std::shared_ptr<point<T>> vertices_[3] = {};
+        triangle_c color_ = {4, 255, 255, 255, 255};
+
+    public:
+        triangle() = default;
+        triangle(point<T>& a, point<T>& b, point<T>& c, triangle_c color = {4, 255, 255, 255, 255});
+
+        point<T>& operator[](vertex v);
+        const point<T>& operator[](vertex v) const;
+        triangle_c color() const;
+        void color(triangle_c color);
+    };
+}
